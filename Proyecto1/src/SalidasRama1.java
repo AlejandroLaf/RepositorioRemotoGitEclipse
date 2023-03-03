0191a8772471ae4d94c241f412beec413e13e2f2
Alex <alafargagatius@iesmordefuentes.com>
2023-03-03 09:42:17 +0100

    "Rama1 salidas de texto"

--- Proyecto1/src/SalidasRama1.java
+++ Proyecto1/src/SalidasRama1.java
@@ -3,6 +3,8 @@ public class SalidasRama1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Primera salida rama1");
+		System.out.println("Segunda salida rama1");
 
 	}
 
